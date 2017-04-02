5b7fe2edd21834d2e4e56344e44f60e4deb386fe
Egrlove <maf2514@gmail.com>
2017-04-03 00:21:30 +0300

    notebook

---
+++ Шаблоны/c++.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+
+
+int int main(int argc, char const *argv[])
+{
+    ;
+}
---
+++ Шаблоны/c.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int int main(int argc, char const *argv[])
+{
+    ;
+}
